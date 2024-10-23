0b77c89588061dd71c797d10f520b9278b38c7e2
Viet Nguyen <vietnguyenswin@outlook.com>
2024-10-23 20:56:35 +1100

    Add main class

---
+++ data-discovery-ai/main.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI, HTTPException, Depends, Security, APIRouter, status
+from fastapi.security import APIKeyHeader
+from common.constants import API_PREFIX, API_KEY_NAME
+from dotenv import load_dotenv
+import os
+
+router = APIRouter(prefix=API_PREFIX)
+
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Load the API key from environment variables
+API_KEY = os.getenv("API_KEY")
+api_key_header = APIKeyHeader(name=API_KEY_NAME)
+
+
+async def api_key_auth(x_api_key: str = Security(api_key_header)):
+    if x_api_key == API_KEY:
+        return x_api_key
+    raise HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Invalid API Key",
+    )
+
+@router.get("/dummy", dependencies=[Depends(api_key_auth)])
+async def hello_world():
+    return {"content": "Hello World!"}
+
+
+app = FastAPI()
+app.include_router(router)
