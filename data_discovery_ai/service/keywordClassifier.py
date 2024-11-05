b0f649a281bbe310ba1cffe26ce16f8508ba2ca0
Yuxuan Hu <lydiahu_hn@hotmail.com>
2024-11-05 14:45:48 +1100

    Revert "change prediction return type to list"
    This reverts commit 888640cf4f73e0cd61e26a80affda52ba67fb3a1.

--- data_discovery_ai/service/keywordClassifier.py
+++ data_discovery_ai/service/keywordClassifier.py
@@ -1,9 +1,9 @@
 import data_discovery_ai.utils.preprocessor as preprocessor
 import data_discovery_ai.model.keywordModel as model
 import configparser
-from typing import List
 
-def keywordClassifier(trained_model: str, description: str) -> List:
+
+def keywordClassifier(trained_model, description):
     """
     The keyword classifier service for API use.
     Input:
@@ -29,4 +29,4 @@ def keywordClassifier(trained_model: str, description: str) -> List:
 
     labels = preprocessor.load_from_file("labels.pkl")
     prediction = model.get_predicted_keywords(target_predicted_labels, labels).to_list()
-    return prediction
+    return " | ".join(prediction)
