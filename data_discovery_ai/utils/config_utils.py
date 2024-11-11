ca80fe2ca652dd6b8a190025f4d6d39ab121d865
Yuxuan Hu <lydiahu_hn@hotmail.com>
2024-11-11 14:28:35 +1100

    remove redundant logic

--- data_discovery_ai/utils/config_utils.py
+++ data_discovery_ai/utils/config_utils.py
@@ -7,28 +7,28 @@ class ConfigUtil:
     def __init__(self) -> None:
         self.base_dif = Path(__file__).resolve().parent.parent
 
-    def load_keyword_config(self) -> configparser.ConfigParser:
+    def _load_config(self, file_name: str) -> configparser.ConfigParser:
         """
-        The util method for load parameters for from a configuration file, which saved as data_discovery_ai/common/keyword_classification_parameters.ini
+        The abstract method to load a configuration file.
         """
-        config_file_path = self.base_dif / "common" / KEYWORD_CONFIG
+        config_file_path = self.base_dif / "common" / file_name
         if not config_file_path.exists():
             raise FileNotFoundError(
                 f"The configuration file was not found at {config_file_path}"
             )
-        keyword_config = configparser.ConfigParser()
-        keyword_config.read(config_file_path)
-        return keyword_config
+        
+        config = configparser.ConfigParser()
+        config.read(config_file_path)
+        return config
+
+    def load_keyword_config(self) -> configparser.ConfigParser:
+        """
+        The util method for load parameters for from a configuration file, which saved as data_discovery_ai/common/keyword_classification_parameters.ini
+        """
+        return self._load_config(KEYWORD_CONFIG)
 
     def load_es_config(self) -> configparser.ConfigParser:
         """
         The util method for load Elasticsearch configurations from a file, which saved as data_discovery_ai/common/esManager.ini
         """
-        elasticsearch_config_file_path = self.base_dif / "common" / ELASTICSEARCH_CONFIG
-        if not elasticsearch_config_file_path.exists():
-            raise FileNotFoundError(
-                f"The configuration file was not found at {elasticsearch_config_file_path}"
-            )
-        esConfig = configparser.ConfigParser()
-        esConfig.read(elasticsearch_config_file_path)
-        return esConfig
+        return self._load_config(ELASTICSEARCH_CONFIG)
