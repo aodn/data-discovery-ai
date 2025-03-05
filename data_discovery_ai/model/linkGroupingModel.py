5ce773eeb2d8d57d901965aac18992e65b9fdd19
Yuxuan Hu <lydiahu_hn@hotmail.com>
2025-03-05 16:42:17 +1100

    add function comment

--- data_discovery_ai/model/linkGroupingModel.py
+++ data_discovery_ai/model/linkGroupingModel.py
@@ -9,6 +9,28 @@ from data_discovery_ai import logger
 
 
 def link_grouping_model(links: List[Dict[str, str]]) -> List[Dict[str, str]]:
+    """
+    Based on link href, title, and content shown on the link, group the links into 'Data Access'/'Document'/'Python Notebook'/ 'Other' categories.
+    Input:
+        links: List[Dict[str, str]]. A list of dictionaries. Each link is a dict with keys 'href', 'rel', 'type', and 'title'. An example likes:
+        {
+              "href": "https://nbviewer.org/github/aodn/aodn_cloud_optimised/blob/main/notebooks/vessel_trv_realtime_qc.ipynb",
+              "rel": "related",
+              "type": "text/html",
+              "title": "Access to Jupyter notebook to query Cloud Optimised converted dataset"
+            }
+        Link grouping model only uses values in 'href' and 'title'. So if a link has no title or no href, it will be skipped.
+    Output:
+        List[Dict[str, str]]. A list of dictionaries. Each link is a dict with keys 'href', 'rel', 'type', 'title', and 'group'. An example likes:
+        {
+              "href": "https://nbviewer.org/github/aodn/aodn_cloud_optimised/blob/main/notebooks/vessel_trv_realtime_qc.ipynb",
+              "rel": "related",
+              "type": "text/html",
+              "title": "Access to Jupyter notebook to query Cloud Optimised converted dataset",
+              "group": "Data Access"
+            }
+        The group of each link is determined by the decision-making rules in constant GROUPING_RULES defined in data_discovery_ai/common/constants.py.
+    """
     # catch empty links
     if not links or len(links) == 0:
         return []
@@ -25,7 +47,13 @@ def link_grouping_model(links: List[Dict[str, str]]) -> List[Dict[str, str]]:
     return links
 
 
-def content_keyword():
+def content_keyword() -> List[str]:
+    """
+    Given a list of keywords set, generate all possible combinations and permutations of the keywords. For example, given ['data', 'dataset'] and ['access'], the function will return:
+    ['data access', 'dataset access', 'access data', 'access dataset']. These keywords are used to filter the content of the link page.
+    Output:
+        List[str]. A list of strings. Each string is a combination of keywords.
+    """
     keyword_groups = GROUPING_RULES["Data Access"]["content"]
     combinations = [" ".join(combo) for combo in product(*keyword_groups)]
     final_combinations = set()
@@ -39,6 +67,14 @@ def content_keyword():
 
 
 def make_decision(link: Dict[str, str], page_content_keywords: List) -> Dict[str, str]:
+    """
+    Based on the decision-making rules defined in GROUPING_RULES, determine the group of the link.
+    Input:
+        link: Dict[str, str]. A dictionary with keys 'href' and 'title'. The link to be grouped.
+        page_content_keywords: List[str]. A list of strings. Each string is a combination of keywords. These keywords are used to filter the content of the link page.
+    Output:
+        str. The group of the link. It can be 'Data Access'/'Document'/'Python Notebook'/ 'Other'.
+    """
     href = link["href"].lower()
     title = link["title"].lower()
 
