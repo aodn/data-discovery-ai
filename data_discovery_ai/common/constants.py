1f3ff2317775411a0cb5c492d2cae7d3720e33e7
Yuxuan Hu <lydiahu_hn@hotmail.com>
2025-03-05 17:05:46 +1100

    update title keywords for data access and remove unnecessory links

--- data_discovery_ai/common/constants.py
+++ data_discovery_ai/common/constants.py
@@ -50,7 +50,7 @@ GROUPING_RULES = {
             "format",
         ],
         # Rule 5: if the title contains data access keywords, then it is a data access link
-        "title": ["access", "download"],
+        "title": ["data access", "download data", "access data", "data download"],
         # Rule 6: if the content contains data access keywords, then it is a data access link, use a combination of keywords as we don't know the exact content
         "content": [["data", "dataset"], ["access", "download"]],
     },
