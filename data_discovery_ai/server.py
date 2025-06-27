c7fe802e9e26d13b598cd8528422a859ce48fbdd
Yuxuan Hu <lydiahu_hn@hotmail.com>
2025-06-27 12:52:31 +1000

    load bert model when init fastapi application

--- data_discovery_ai/server.py
+++ data_discovery_ai/server.py
@@ -5,13 +5,18 @@ from transformers import AutoTokenizer, TFBertModel
 from contextlib import asynccontextmanager
 
 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
+def load_tokenizer_model():
     # https://huggingface.co/docs/transformers/v4.47.1/en/model_doc/bert#transformers.TFBertModel
     tokenizer = AutoTokenizer.from_pretrained("google-bert/bert-base-uncased")
     # use in Tensorflow https://huggingface.co/google-bert/bert-base-uncased
     embedding_model = TFBertModel.from_pretrained("google-bert/bert-base-uncased")
 
+    return tokenizer, embedding_model
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    tokenizer, embedding_model = load_tokenizer_model()
     app.state.tokenizer = tokenizer
     app.state.embedding_model = embedding_model
     yield
