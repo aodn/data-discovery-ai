bea1bfbc398ec085122e061608f47f79bc214f84
Yuxuan Hu <lydiahu_hn@hotmail.com>
2025-06-30 13:33:26 +1000

    update keyword agent response format and add test case

--- tests/agents/test_keyword_classification_agent.py
+++ tests/agents/test_keyword_classification_agent.py
@@ -2,7 +2,9 @@
 import unittest
 from data_discovery_ai.agents.keywordClassificationAgent import (
     KeywordClassificationAgent,
+    reformat_response,
 )
+from data_discovery_ai.ml.preprocessor import ConceptTheme, Concept
 
 
 class TestKeywordClassificationAgent(unittest.TestCase):
@@ -13,8 +15,62 @@ class TestKeywordClassificationAgent(unittest.TestCase):
 
         self.invalid_request = {"title": "Test Title"}
 
+        self.prediciton = {
+            "themes": [
+                {
+                    "id": "Concept1",
+                    "url": "http://example.com/concept1",
+                    "theme": {
+                        "title": "Theme A",
+                        "scheme": "Scheme A",
+                        "description": "Desc A",
+                    },
+                },
+                {
+                    "id": "Concept2",
+                    "url": "http://example.com/concept2",
+                    "theme": {
+                        "title": "Theme A",
+                        "scheme": "Scheme A",
+                        "description": "Desc A",
+                    },
+                },
+                {
+                    "id": "Concept3",
+                    "url": "http://example.com/concept3",
+                    "theme": {
+                        "title": "Theme B",
+                        "scheme": "Scheme B",
+                        "description": "Desc B",
+                    },
+                },
+            ]
+        }
+
     def test_make_decision_valid_request(self):
         # it should always take action if the request is valid, otherwise it will not take action.
         self.assertTrue(self.agent.make_decision(self.valid_request))
 
         self.assertFalse(self.agent.make_decision(self.invalid_request))
+
+    def test_reformat_response(self):
+        formated_response = reformat_response(self.prediciton)
+        self.assertIn("themes", formated_response)
+        # expected to have two themes A and B
+        self.assertEqual(len(formated_response["themes"]), 2)  # Two themes expected
+
+        for theme in formated_response["themes"]:
+            self.assertIn("concepts", theme)
+            self.assertIn("scheme", theme)
+            self.assertIn("title", theme)
+            self.assertIn("description", theme)
+            # test if AI prediction field tagged
+            self.assertEqual(
+                theme["ai:description"], "This is the prediction provided by AI model"
+            )
+
+        concepts_by_theme = {
+            t["title"]: t["concepts"] for t in formated_response["themes"]
+        }
+        self.assertEqual(len(concepts_by_theme["Theme A"]), 2)
+        self.assertEqual(len(concepts_by_theme["Theme B"]), 1)
